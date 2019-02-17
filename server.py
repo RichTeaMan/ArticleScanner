e33aaa1f24de0de8f74597449c5cbde7892480f8
Thomas Holmes <thomasholmes@outlook.com>
2019-02-17 16:33:36 +0000

    Renamed files to be lowercase.

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from json import dumps
-from Scanner import Scanner
+from scanner import Scanner
 
 app = Flask(__name__)
 api = Api(app)
