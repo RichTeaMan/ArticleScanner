7a3b7f1492c67d705ff6fafe70a1d0964bb04a19
Thomas Holmes <thomasholmes@outlook.com>
2019-04-27 13:34:47 +0100

    Changed API to use form parameter.

--- server.py
+++ server.py
@@ -7,17 +7,13 @@ app = Flask(__name__)
 api = Api(app)
 scanner = Scanner()
 
-class Test(Resource):
-    def get(self):
-        return {'employees': 'test'} # Fetches first column that is Employee ID
-    
+class Scan(Resource):    
     def post(self):
-        json_data = request.get_json(force=True)
-        doc = json_data['doc']
-        tokens = scanner.scan(doc)
+        document = request.form['document']
+        tokens = scanner.scan(document)
         return jsonify(tokens=[e.serialize() for e in tokens])
 
-api.add_resource(Test, '/test') # Route_1
+api.add_resource(Scan, '/scan') # Route_1
 
 if __name__ == '__main__':
      app.run(host='0.0.0.0', port='5002')
